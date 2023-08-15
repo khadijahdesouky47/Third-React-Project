05cc07cc5d388409969b544fa2e4a982abf0d709
khadijahdesouky47 <khadijahdesouky47@gmail.com>
2023-08-15 14:11:25 +0300

    Updates

--- src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import elclass from './Card.module.css';
-
-function Card (props) {
-    return <div className={`${elclass.card} ${props.className}`}>
-        {props.children}
-    </div>
-}
-export default Card;
